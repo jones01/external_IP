ef48bd1e99a6fa2116d138070aa6d5b2df738e4e
Octavis Jones <octavis@arin.net>
2016-11-08 10:46:47 -0500

    new veriables

--- monitorWebsite.sh
+++ monitorWebsite.sh
@@ -3,13 +3,17 @@
 # monitor.sh - Monitors a web page for changes
 # sends an Mac notification if the file change
 # By:Octavis Jones 4/4/16
+# v2 - 11/8/2016
+#    - output veriable name to notification
 #
 ##################################################
 
 # Global Declarations
 declare -r SCRIPT="${0##*/}"
-cmd_list='osascript curl diff mv grep' # Reference Commands
-path="/Users/octavis/scripts"
+cmd_list='osascript curl diff mv grep touch' # Reference Commands
+path="/Users/octavis/scripts/site_test/"
+title="Website Update"
+
 
 cmd_exists() {  # Function to check if referenced command exists
   if [ $# -eq 0 ]; then
@@ -30,23 +34,26 @@ done
 check_site(){
   oldsite="$2_old.txt"
   newsite="$2_new.txt"
+  touch "$path/$newsite" "$path/$oldsite"
   mv "$path/$newsite" "$path/$oldsite" 2> /dev/null
   echo "Downloading: $2"
-  if [[ $1 == *"jamfnation"* ]]
-    then
+  if [[ $1 == *"jamf.com"* ]]; then
       curl $1 -L -s --compressed | grep "Last Updated:" -A 1 > $path/$newsite
     else
       curl $1 -L --compressed -s > "$path/$newsite"
   fi
   echo "Compairing: $2"
-  DIFF_OUTPUT="$(diff $newsite $oldsite)"
+  message="There has been a change to $2. Go Check!"
+  DIFF_OUTPUT="$(diff site_test/$newsite site_test/$oldsite)"
   if [ "0" != "${#DIFF_OUTPUT}" ]; then
-  osascript -e 'display notification "There has been a change to one or more websites. Go Check! "'
+    osascript -e "display notification \"$message\" with title \"$title\" "
   fi
  }
 
 
 ### Function calls - Check sites
 check_site 'http://macadmins.software/' MSOffice2016
-check_site 'https://jamfnation.jamfsoftware.com/viewProduct.html?id=41' JAMF_AdobeFlash
-check_site 'https://jamfnation.jamfsoftware.com/viewProduct.html?id=492' JAMF_Slack
+check_site 'https://app-updates.agilebits.com/product_history/OPM4' 1Password
+check_site 'https://cyberduck.io/changelog/' Cyberduck
+check_site 'https://www.jamf.com/jamf-nation/third-party-products/41/flash-player?view=info' JAMF_AdobeFlash
+check_site 'https://www.jamf.com/jamf-nation/third-party-products/492/slack?view=info' JAMF_Slack
