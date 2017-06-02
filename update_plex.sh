8d0a48b729f876c256830dceaea8996577e2d5f4
jones01 <octavis.jones@gmail.com>
2017-06-02 08:30:30 -0400

    Create version1

---
+++ update_plex.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Downloads version of Plex you provided to current Dir
+# installs,restarts then removes file
+#
+#################################
+
+
+# Global Declarations
+declare -r SCRIPT="${0##*/}"
+cmd_list='rpm curl systemctl rm' # Reference Commands
+version="1.7.2.3878-8088811b8" # Version to upgrade to
+
+cmd_exists() {  # Function to check if referenced command exists
+  if [ $# -eq 0 ]; then
+    echo 'No command argument was passed to verify exists'
+    exit 190
+  fi
+  cmd=${1}
+  cmd_fullpath=$(which "${cmd}")
+  if [ ! -x "${cmd_fullpath}" ]; then
+    printf "${SCRIPT}: the command ${cmd} not found - aborting\n"
+    exit 191
+  fi
+}
+for cmd in ${cmd_list}; do   # Verify that referenced commands exist on the system
+  cmd_exists "$cmd"
+done
+##################
+
+
+#download plex version listed above
+ curl -o plexmediaserver-"$version".x86_64.rpm 'https://downloads.plex.tv/plex-media-server/'$version'/plexmediaserver-'$version'.x86_64.rpm'
+
+#install new Plex version
+ sudo rpm -Uvh plexmediaserver-"$version".x86_64.rpm
+
+#restart the PLEX service
+ systemctl start plexmediaserver.service
+
+#cleanup/remove installer
+ rm plexmediaserver-"$version".x86_64.rpm
+
+echo " === Complete ==== "
